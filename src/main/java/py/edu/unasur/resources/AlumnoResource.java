4d126de9614768c47af0d7a60b72c5fe602e5f65
Vieraal <alfabianviera94@gmail.com>
2024-12-02 22:15:35 -0300

    se encontro un error en este punto
    Cambie el archivo

--- src/main/java/py/edu/unasur/resources/AlumnoResource.java
+++ src/main/java/py/edu/unasur/resources/AlumnoResource.java
@@ -1,7 +1,6 @@
-package  py.edu.unasur.resources;
+package py.edu.unasur.resources;
 
 import java.util.List;
-
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
@@ -15,40 +14,42 @@ import py.edu.unasur.services.AlumnoService;
 import py.edu.unasur.services.GastosArchivoService;
 
 @Path("alumno")
-public  class AlumnoResource {
-
+public class AlumnoResource {
 
     private final AlumnoService service;
-    
     private final GastosArchivoService gastosArchivoService;
-    
+
     public AlumnoResource(AlumnoService service, GastosArchivoService gastosArchivoService){
-        this.gastosArchivoService = gastosArchivoService;
         this.service = service;
+        this.gastosArchivoService = gastosArchivoService;
     }
 
     @GET
     @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Alumno ejemploDeRepository(@PathParam("id")Integer id) {
+    public Alumno ejemploDeRepository(@PathParam("id") Integer id) {
         return this.service.obtener(id);
     }
+
     @DELETE
     @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public void eliminarDatoArchivo(@PathParam("id")Integer id) {
+    public void eliminarDatoArchivo(@PathParam("id") Integer id) {
+        // Podrías usar gastosArchivoService aquí si se requiere
+        this.gastosArchivoService.eliminarArchivo(id); // Ejemplo de uso
         this.service.eliminar(id);
     }
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Alumno> listarDatosRepository() {
         return this.service.listar();
     }
+
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     public Response guardarDatoRepository(Alumno param) {
-            this.service.guardar(param);
+        this.service.guardar(param);
         return Response.ok().build();
     }
-
 }
